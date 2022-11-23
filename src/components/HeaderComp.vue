3d641a626b006a469cd222cd06dfb589bfdb39d0
AndreaPanzeri <andpanz@libero.it>
2022-11-23 16:00:23 +0100

    HeaderComp

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container">
+    <div class="navbar">
+      <div><img src="../assets/img/dc-logo.png" alt="logo" /></div>
+      <div class="lista">
+        <ul>
+          <li><a href="#">CHARACTERS</a></li>
+          <li><a href="#">COMICS</a></li>
+          <li><a href="#">MOVIES</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="content">
+      <h2>--Content goes here-- </h2>
+    </div>
+  </div>
+</template>
+  
+
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+  
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+/********************
+       COMMON
+*********************/
+
+
+
+
+.container{
+  background-color: white;
+}
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin: auto;
+}
+
+/*****************************
+               LISTA
+*******************************/
+
+.lista {
+  align-self: center;
+}
+
+ul {
+  list-style-type: none;
+  display: flex;
+}
+
+li {
+  margin: auto 10px;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+a:hover {
+  color: rgb(64, 174, 211);
+  border-bottom: 5px solid rgb(64, 174, 211);
+  padding-bottom: 25px;
+}
+
+/********************
+       CONTENT
+*********************/
+.content{
+  background-color: #1f1f1f;
+}
+
+.content h2{
+  color: white;
+  margin: 0;
+  padding-top: 3rem;
+  width: 80%;
+  margin: auto;
+  font-size: 2rem;
+}
+</style>
+
+
+
